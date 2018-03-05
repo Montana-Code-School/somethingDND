f7bc700b8df31d074d54c36cc85615ba3a11b1fd
Eli Dery <elidery@MacBook-Pro-3.local>
2018-03-05 16:12:54 -0700

    shows a race and class

--- generator/src/App.js
+++ generator/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BuildButton from './BuildButton.js';
+import _ from 'lodash';
 
 class App extends Component {
   constructor(props) {
@@ -33,19 +34,39 @@ class App extends Component {
           fetching: false
         });
       })
+
+
+  fetch('/api/class')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`status ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(json => {
+      console.log(json)
+      this.setState({
+        classtype: json,
+        fetching: false
+      });
+    }).catch(e => {
+      this.setState({
+        classtype: `API call failed: ${e}`,
+        fetching: false
+      });
+    })
   }
 
+
   render() {
-    if(this.state.race.length === 0) {
-        return (<p>loading</p>);
-    } else {
      return (
       <div className="App">
-        <p>{this.state.race[0].name}</p>
-        <p>{this.state.race[0].subraces[1].name}</p>
+        <p>{_.has(this.state.race[0], 'name') ? this.state.race[0].name : 'loading'}</p>
+        <p>{_.has(this.state.race[0], 'subraces[1].name') ? this.state.race[0].subraces[1].name : 'loading'}</p>
+        <p>{_.has(this.state.classtype[0], 'name') ? this.state.classtype[0].name : 'loading'}</p>
+        <p>{_.has(this.state.classtype[0], 'subclasses[0].name' ) ? this.state.classtype[0].subclasses[0].name : 'loading'}</p>
         </div>
       );
-    }
   }
 }
 
