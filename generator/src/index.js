eb433f5da0f89fd9e14adcff5d31cca5415735b6
Eli Dery <elidery@MacBook-Pro-3.local>
2018-03-21 15:24:58 -0600

    added home and character link pages and added title to home page

--- generator/src/index.js
+++ generator/src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App.js';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render((
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>
+), document.getElementById('root'));
 registerServiceWorker();
 // heroku DB: mongodb://heroku_fw6bf6l1:kpl68f0vcqgnn6vjo0on0c0jg1@ds151528.mlab.com:51528/heroku_fw6bf6l1
 // our API Key: V1QnqSjvq08cxgx8FtSFsILgZBJCap-D
